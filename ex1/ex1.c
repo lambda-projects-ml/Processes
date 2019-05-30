8cfa9f809f2f18b3121d80d9dbdc02a8a089d994
Mike Landers <landers.mike@gmail.com>
2019-05-30 13:38:19 -0700

    Completed first two

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,10 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
+// (e.g., x) and set its value to something (e.g., 100).
+
+// What value is the variable in the child process?
+// Currently set to take the original value
 // What happens to the variable when both the child and parent change the value of x?
+// They each have different values for the variable. One cannot change it for the other
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +12,24 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf("Fork Failed");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        // x = 10;
+        printf("Child: %d\n", x);
+    }
+    else
+    {
+        x = 50;
+        printf("Parent: %d\n", x);
+    }
 
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,11 @@
-// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory
+// and then calls `fork()` to create a new process.
+
+//Can both the child and parent access the file descriptor returned by `fopen()`?
+// Yes. They both are able to read and write to the same file
+
+//What happens when they are written to the file concurrently?
+// They write in the order they are processed
 
 #include <stdio.h>
 #include <unistd.h>
@@ -8,7 +13,23 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE *fp;
+    fp = fopen("text.txt", "w+");
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        printf("Failed");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        fprintf(fp, "%s\n", "Child");
+    }
+    else
+    {
+        fprintf(fp, "%s\n", "Parent");
+    }
+
     return 0;
 }
